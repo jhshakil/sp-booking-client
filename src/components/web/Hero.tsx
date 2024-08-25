151f8017c6e671eceeb80f1fc2cf26cf0011373d
jhshakil <shakil11271@gmail.com>
2024-08-25 22:48:24 +0600

    complete hero section

--- src/components/web/Hero.tsx
+++ src/components/web/Hero.tsx
@@ -3,14 +3,22 @@ import { Button } from "../ui/button";
 const Hero = () => {
   return (
     <div>
-      <div
-        className="bg-cover bg-center bg-no-repeat h-[70vh] relative before:absolute before:w-full before:h-full before:top-0 before:left-0 before:bg-[#0000006e]"
-        style={{ backgroundImage: "url(/images/banner/hero-banner.jpg)" }}
-      >
-        <div className="container flex flex-col items-center justify-center h-full relative z-10 text-white ">
-          <h1 className="text-[60px] font-bold">Find Next Game To Play</h1>
-          <p className="text-2xl">Discover amazing places at exclusive deals</p>
-          <Button className="mt-9 bg-orange-600">Book Now</Button>
+      <div className="relative">
+        <img
+          src="/images/banner/hero-banner.jpg"
+          className="w-full h-[70vh] object-cover"
+          alt="hero-banner"
+        />
+        <div className="absolute bottom-0 w-full">
+          <div className="max-w-full md:max-w-[550px] lg:max-w-[800px] mx-auto flex flex-col items-center justify-center h-full relative z-10 text-black bg-white px-4 md:px-8 py-3 md:py-11 rounded-t-xl">
+            <h1 className="text-[40px] lg:text-[60px] font-bold text-center leading-snug">
+              Find Next Game To Play
+            </h1>
+            <p className="text-lg lg:text-2xl text-center mt-3">
+              Discover amazing places at exclusive deals
+            </p>
+            <Button className="mt-9 bg-orange-600 text-lg">Book Now</Button>
+          </div>
         </div>
       </div>
     </div>
