74388dfbe3063b405e1ee1b13b0dd9e6c955e68b
jhshakil <shakil11271@gmail.com>
2024-08-25 21:47:11 +0600

    complete navigation

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,33 +3,40 @@ import { cn } from "@/lib/utils";
 import { buttonVariants } from "../ui/button";
 import Navigation from "./Navigation";
 import ProfileNavigation from "./ProfileNavigation";
+import MobileNavigation from "./MobileNavigation";
 
 const Header = () => {
   return (
-    <div className="container px-3 py-4 flex justify-between items-center gap-11">
+    <div className="container px-3 py-4 flex justify-between items-center gap-6 lg:gap-11">
       {/* logo  */}
       <div>
         <img src="/images/logo/logo.png" width={55} alt="logo" />
       </div>
       {/* nav  */}
-      <div className="flex-1">
+      <div className="hidden md:block flex-1">
         <Navigation />
       </div>
       {/* action button  */}
       <div className="flex gap-4 items-center">
         <div className="flex items-center gap-4">
-          <Link
-            to="/login"
-            className={cn(buttonVariants({ variant: "outline" }))}
-          >
+          <Link to="/login" className={cn(buttonVariants())}>
             Login
           </Link>
-          <Link to="/registration" className={cn(buttonVariants())}>
+          <Link
+            to="/registration"
+            className={cn(
+              buttonVariants({ variant: "outline" }),
+              "hidden lg:block"
+            )}
+          >
             Registration
           </Link>
         </div>
-        <div>
+        <div className="flex items-center gap-4">
           <ProfileNavigation />
+          <div className="md:hidden">
+            <MobileNavigation />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/header/MobileNavigation.tsx
@@ -0,0 +1,76 @@
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+import { Menu, X } from "lucide-react";
+import { Link } from "react-router-dom";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "../ui/navigation-menu";
+import { cn } from "@/lib/utils";
+
+const MobileNavigation = () => {
+  return (
+    <Drawer direction="left">
+      <DrawerTrigger asChild>
+        <Menu />
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto w-full h-full">
+          <DrawerHeader className="flex justify-end">
+            <DrawerClose asChild>
+              <X />
+            </DrawerClose>
+          </DrawerHeader>
+          <div>
+            <NavigationMenu>
+              <NavigationMenuList>
+                <NavigationMenuItem className="flex flex-col gap-8">
+                  <NavigationMenuLink
+                    className={cn(navigationMenuTriggerStyle(), "text-2xl")}
+                    asChild
+                  >
+                    <Link to="/">Home</Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink
+                    className={cn(navigationMenuTriggerStyle(), "text-2xl")}
+                    asChild
+                  >
+                    <Link to="/facility">Facility</Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink
+                    className={cn(navigationMenuTriggerStyle(), "text-2xl")}
+                    asChild
+                  >
+                    <Link to="/booking">Booking</Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink
+                    className={cn(navigationMenuTriggerStyle(), "text-2xl")}
+                    asChild
+                  >
+                    <Link to="/about">About Us</Link>
+                  </NavigationMenuLink>
+                  <NavigationMenuLink
+                    className={cn(navigationMenuTriggerStyle(), "text-2xl")}
+                    asChild
+                  >
+                    <Link to="/contact">Contact Us</Link>
+                  </NavigationMenuLink>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
+          </div>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+
+export default MobileNavigation;
